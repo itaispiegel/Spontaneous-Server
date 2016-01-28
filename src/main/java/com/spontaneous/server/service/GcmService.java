d6e624752589255deadccfd3d06a4363528e2e7c
Itai Spiegel <itai.spiegel@gmail.com>
2016-01-28 16:49:15 +0200

    Notifications

--- src/main/java/com/spontaneous/server/service/GcmService.java
+++ src/main/java/com/spontaneous/server/service/GcmService.java
@@ -2,10 +2,7 @@ package com.spontaneous.server.service;
 
 import com.google.android.gcm.server.Message;
 import com.google.android.gcm.server.Sender;
-import com.spontaneous.server.model.entity.Event;
 import com.spontaneous.server.model.entity.InvitedUser;
-import com.spontaneous.server.model.entity.Notification;
-import com.spontaneous.server.model.entity.User;
 import org.hibernate.service.spi.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,32 +20,20 @@ public class GcmService {
     private static final String API_KEY = "AIzaSyCZNSEfuv0Lk8AREq9avNappUouZwWtI6I";
 
     private final Logger mLogger = LoggerFactory.getLogger(this.getClass());
-    private final UserService mUserService;
-
-    @Autowired
-    public GcmService(UserService userService) {
-        this.mUserService = userService;
-    }
 
     /**
-     * Send a notification to a given user.
+     * Send a notificationType to a given user.
      *
-     * @param notification The notification data.
-     * @param userId       Id of the user we wish to send the notification to.
+     * @param message        The notification details.
+     * @param recipientToken The GCM token of the recipient.
      * @throws ServiceException Is thrown in case that no user is found.
-     * @throws IOException      Is thrown in case that the notification was not sent.
+     * @throws IOException      Is thrown in case that the notificationType was not sent.
      */
-    private void sendNotification(Notification notification, long userId) throws ServiceException, IOException {
-        User recipient = mUserService.getUserById(userId);
-
-        mLogger.info(String.format("Sending a notification to user with id #%d", userId));
-
-        Message message = new Message.Builder()
-                .addData("notification", notification.toString())
-                .build();
+    private void sendNotification(Message message, String recipientToken) throws ServiceException, IOException {
+        mLogger.info(String.format("Notifying user with token #%s", recipientToken));
 
         Sender sender = new Sender(API_KEY);
-        sender.sendNoRetry(message, recipient.getGcmToken());
+        sender.sendNoRetry(message, recipientToken);
     }
 
     /**
@@ -60,11 +45,21 @@ public class GcmService {
         final String messageTitle = "Spontaneous";
         final String messageContent = "You have been invited to an event!";
 
-        Notification<Event> notification = new Notification<>(messageTitle, messageContent,
-                Notification.NotificationType.INVITATION, invitedUser.getEvent());
+        Message message = new Message.Builder()
+                .addData("type", NotificationType.INVITATION.toString())
+                .addData("title", messageTitle)
+                .addData("content", messageContent)
+                .addData("data", invitedUser.getEvent().toString())
+                .build();
 
-        sendNotification(notification, invitedUser.getUser()
-                .getId());
+        sendNotification(message, invitedUser.getUser()
+                .getGcmToken());
     }
 
+    /**
+     * This class represents the type of notificationType sent to the user's device.
+     */
+    enum NotificationType {
+        INVITATION
+    }
 }
