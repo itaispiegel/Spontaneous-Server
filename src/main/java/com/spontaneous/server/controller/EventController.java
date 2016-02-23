ee89826f0f81f8345ae9f010a7c4f774996b9a39
Itai Spiegel <itai.spiegel@gmail.com>
2016-02-23 18:27:16 +0200

    a

--- src/main/java/com/spontaneous/server/controller/EventController.java
+++ src/main/java/com/spontaneous/server/controller/EventController.java
@@ -69,7 +69,7 @@ public class EventController {
             Event event = mEventService.updateEvent(id, saveEventRequest);
             return new BaseResponse<>(event);
 
-        } catch (ServiceException e) {
+        } catch (ServiceException | IOException e) {
             return new BaseResponse<>(e.getMessage(), BaseResponse.INTERNAL_ERROR);
         }
     }
